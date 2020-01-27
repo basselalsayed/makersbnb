f34dabc05f55072400850bff29407919b09cb67c
basselalsayed <alsayed.bassel@gmail.com>
2020-01-27 18:03:11 +0000

    end of days work

---
+++ public/interface.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#signUpButton").click(function() {
+   $("#signUpForm").toggle();
+  });
+});
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,13 @@
+feature '/' do
+  scenario 'contains info and buttons required' do 
+    visit '/'
+    expect(page).to have_button 'Sign Up'
+    expect(page).to have_content 'BnB'
+  end
+
+  scenario 'clicking Sign Up shows the Form' do
+    visit '/'
+    click_button('Sign Up')
+    expect(page).to have_button 'Submit'
+  end
+end
