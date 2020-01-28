60379a56400e080bc9c33a96b6c95ecbbd358ae9
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-28 17:19:28 +0000

    readme reflection added

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -21,9 +21,9 @@ feature '/' do
     expect(page).to have_content '20TE5T'
   end
 
-  scenario 'will show post code on page' do
-    visit '/'
-    expect(page).to have_content 'https://www.homesud.co.uk/rentals/fotos/2/1449730608e751943fdac459e3553b15523549ad43/1479152770e7045c48ff0a57756b17c9b41bd5295c.jpg'
-  end
+  # scenario 'will show post code on page' do
+  #   visit '/'
+  #   expect(page).to have_content 'https://www.homesud.co.uk/rentals/fotos/2/1449730608e751943fdac459e3553b15523549ad43/1479152770e7045c48ff0a57756b17c9b41bd5295c.jpg'
+  # end
 
 end
