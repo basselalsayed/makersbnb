0085164902ed8757b284b88dcb945b7400a9cd36
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-28 17:04:58 +0000

    added rspec tests

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,5 +1,5 @@
 feature '/' do
-  scenario 'contains info and buttons required' do 
+  scenario 'contains info and buttons required' do
     visit '/'
     expect(page).to have_button 'Sign Up'
     expect(page).to have_content 'BnB'
@@ -14,6 +14,16 @@ feature '/' do
   scenario 'will show listing of properties' do
     visit '/'
     expect(page).to have_content '20 test avenue'
+  end
+
+  scenario 'will show post code on page' do
+    visit '/'
+    expect(page).to have_content '20TE5T'
+  end
+
+  scenario 'will show post code on page' do
+    visit '/'
+    expect(page).to have_content 'https://www.homesud.co.uk/rentals/fotos/2/1449730608e751943fdac459e3553b15523549ad43/1479152770e7045c48ff0a57756b17c9b41bd5295c.jpg'
   end
 
 end
