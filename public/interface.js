e252841bc3af4a06aa8104e5c69f9f1999f0e1e2
Jakephillips55 <Jakep629@gmail.com>
2020-01-29 15:46:32 +0000

    popup is bugged but functions

--- public/interface.js
+++ public/interface.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-  $("#signUpButton").click(function() {
-   $("#signUpForm").toggle();
-  });
-});
+// $(document).ready(function() {
+//   $("#signUpButton").click(function() {
+//    $("#signUpForm").toggle();
+//   });
+
+  $(document).ready(function() {
+    $("#signUpButton").click(function() {
+    $("#signUpForm").toggle();
+    });
+    $( "#datepicker" ).datepicker();
+    $( "#datepicker2" ).datepicker();
+    $("#OpenDialog").click(function () {
+     $("#dialog").dialog({modal: true, height: 500, width: 700 });
+   });
+ });
+
